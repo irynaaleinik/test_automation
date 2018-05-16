fe79d8679299b9662a79b6847e55ea4260af2bed
Iryna_Aleinik <iryna_aleinik@epam.com>
2018-05-16 20:13:46 +0300

    Open Award plus from: My Nomination, News Feed, New User Profile, New My Activity 3 browsers: Chrome, FF, Opera

--- src/test/java/com/globoforce/mentoring/testautomation/awardlightbox/OpenFullScreenAward.java
+++ src/test/java/com/globoforce/mentoring/testautomation/awardlightbox/OpenFullScreenAward.java
@@ -2,11 +2,8 @@ package com.globoforce.mentoring.testautomation.awardlightbox;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
-import java.util.concurrent.TimeUnit;
 
 public class OpenFullScreenAward {
     private WebDriver driver;
@@ -15,25 +12,26 @@ public class OpenFullScreenAward {
     private String password = "pass";
     private String awardTitle = "Test Award +";
 
+    private String myDashboardMenuLocator = "//div[@id='navBar']//a[@href='/microsites/t/dashboard/MyActivity?client=recipientbased']";
+    private String homeMenuLocator = "//div[@id='navBar']//a[@href='/microsites/t/home?client=recipientbased']";
+
     @Parameters({ "driverName", "path" })
     @BeforeClass
-    public void startDriver(String driverName, String path) {
-        System.setProperty(driverName, path);
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    public void startDriverAndLogin(@Optional("chrome") String driverName, String path) {
+        Utils utils = new Utils(driverName, path);
+        driver = utils.setDriver();
         driver.manage().window().maximize();
         driver.navigate().to(URL);
         driver.findElement(By.name("username")).sendKeys(userName);
         driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.id("signIn-button")).click();
-        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='navBar']//a[@href='/microsites/t/home?client=recipientbased']")).getText(), "Home");
     }
 
     @Test
     public void openAwardPlusFromMyNomination(){
-        driver.findElement(By.xpath("//div[@id='navBar']//a[@href='/microsites/t/dashboard/MyActivity?client=recipientbased']")).click();
+        driver.findElement(By.xpath(myDashboardMenuLocator)).click();
         driver.findElement(By.xpath("//ul[@id='subNavBar']//a[@href='/microsites/t/dashboard/MyNominations?client=recipientbased']")).click();
-        driver.findElement(By.cssSelector("td.col-icons a.detailsLink")).click();
+        driver.findElement(By.cssSelector("div#my_nomination tbody tr:last-child td.col-icons a.detailsLink")).click();
         driver.findElement(By.id("viewAward")).click();
         Assert.assertEquals(driver.findElement(By.cssSelector("div#unifiedAwardDetailsLightbox div#al-messageEditorContainer h2")).getText(),awardTitle,
                 "Award title not found or different");
@@ -41,8 +39,25 @@ public class OpenFullScreenAward {
 
     @Test(priority = 1)
     public void openAwardPlusFromNewsFeed(){
-        driver.findElement(By.xpath("//div[@id='navBar']//a[@href='/microsites/t/home?client=recipientbased']")).click();
+        driver.findElement(By.xpath(homeMenuLocator)).click();
         driver.findElement(By.xpath("//h3[contains(text(),'" + awardTitle + "')]/following-sibling::div[contains(@class,'buttonStyle1')]//a[@id='viewAwardButton1-button']")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#unifiedAwardDetailsLightbox div#al-messageEditorContainer h2")).getText(),awardTitle,
+                "Award title not found or different");
+    }
+
+    @Test(priority = 2)
+    public void openAwardPlusFromNewMyActivity(){
+        driver.findElement(By.xpath(myDashboardMenuLocator)).click();
+        driver.findElement(By.xpath("//p[contains(text(),'" + awardTitle + "')]/following-sibling::a")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#unifiedAwardDetailsLightbox div#al-messageEditorContainer h2")).getText(),awardTitle,
+                "Award title not found or different");
+    }
+
+    @Test(priority = 3)
+    public void openAwardFromNewUserProfile(){
+        driver.findElement(By.xpath(homeMenuLocator)).click();
+        driver.findElement(By.xpath("//h3[contains(.,'" + awardTitle + "')]/ancestor::div[contains(@class, 'awardNewsItem')]/descendant::h3/child::a")).click();
+        driver.findElement(By.xpath("//p[contains(text(),'" + awardTitle + "')]/following-sibling::a")).click();
         Assert.assertEquals(driver.findElement(By.cssSelector("div#unifiedAwardDetailsLightbox div#al-messageEditorContainer h2")).getText(),awardTitle,
                 "Award title not found or different");
     }
@@ -52,4 +67,5 @@ public class OpenFullScreenAward {
         if (driver != null)
             driver.quit();
     }
+
 }
